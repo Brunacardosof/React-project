beffd0dc1dadb667c2fb070001b610dc9f0262a3
Bruna <Brunacardosofurtado@gmail.com>
2025-08-21 17:04:22 +0100

    Criando botoes com props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import Header from '../comopents/Header/Header'
 import Footer from '../comopents/Footer/Footer'
+import Button from '../comopents/Button/Button'
+
 
 function Home() {
     return (
@@ -7,6 +9,10 @@ function Home() {
            <Header />
            <h1>Home</h1>
            <p>Subtitle</p>
+           <Button buttonStyle="primary" arrow >Ola</Button>
+           <Button buttonStyle="secondary" arrow >Ola</Button>
+           <Button buttonStyle="outline" >Ola</Button>
+           <Button buttonStyle="primary" disabled >Ola</Button>
            <Footer />
         </>
     )
